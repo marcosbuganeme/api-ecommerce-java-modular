518cf895acd7d0402cc095759817c551df2654d2
Marcos O. S. Buganeme <marcos.after@gmail.com>
2019-03-24 22:59:30 -0300

    module ecommerce-domain

---
+++ ecommerce-parent/ecommerce-domain/src/main/java/br/com/ecommerce/java/domain/EcommerceDomainApplication.java
@@ -0,0 +1,13 @@
+package br.com.ecommerce.java.domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceDomainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceDomainApplication.class, args);
+	}
+
+}
---
+++ ecommerce-parent/ecommerce-domain/src/test/java/br/com/ecommerce/java/domain/EcommerceDomainApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.ecommerce.java.domain;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class EcommerceDomainApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
