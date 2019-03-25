cb0df4913809408ab875858ddad613eea87555ce
Marcos O. S. Buganeme <marcos.after@gmail.com>
2019-03-24 22:59:45 -0300

    module ecommerce-web

---
+++ ecommerce-parent/ecommerce-web/src/main/java/br/com/ecommerce/java/web/ecommerceweb/EcommerceWebApplication.java
@@ -0,0 +1,13 @@
+package br.com.ecommerce.java.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceWebApplication.class, args);
+	}
+
+}
